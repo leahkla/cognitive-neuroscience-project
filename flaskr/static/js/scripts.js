717b32a0ce9da3f2fae046bae0307c3b956bde58
Lauri Lode <lauri.lode@gmail.com>
2019-02-21 19:44:40 +0200

    Timestamps and sliders

--- flaskr/static/js/scripts.js
+++ flaskr/static/js/scripts.js
@@ -5,9 +5,19 @@ $(function () {
     min: 0,
     max: 100,
     value: 60,
+    timestamp: player.getTimeStamp(),
     slide: function (event, ui) {
       $("#amount").val(ui.value);
+    },
+    stop: function(event, ui) {
+      $("#time").val(ui.timestamp);
     }
   });
   $("#amount").val($("#slider-vertical").slider("value"));
+/* 
+  $("#time").val($("#slider-vertical")).slider("time"));
+    range: "min",
+    stop: function( event, ui ) {player.getTimeStamp());}
+  
+  }); */
 });
