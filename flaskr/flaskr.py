0837de0fcb71c21ef04d4e69e12934c456a12ee0
Lea K <lea.kosonen@helsinki.fi>
2019-02-22 14:35:15 +0200

    created index html and css files, modified flaskr.py by adding chart function for line chart

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,5 +1,8 @@
 import os
 import json
+import pandas as pd
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, show, output_file
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
@@ -22,6 +25,45 @@ def save():
     videoname = request.form.get('videoname')
     d.insert_post({"timestamp":t, "value": v, "videoname": videoname})
     return "Saving completed"
+
+@app.route('/researcher_view', methods=['GET'])
+def chart():
+    # mock data:
+    data2 = {"objects": [{"timestamp": "1", "value": "-0.5", "videoname": "dog video"},
+                    {"timestamp": "3.5", "value": "1", "videoname": "cat video"},
+                    {"timestamp": "5", "value": "-1", "videoname": "rat video"},
+                    {"timestamp": "6.5", "value": "0.7", "videoname": "capybara video"},
+                    {"timestamp": "10.2", "value": "0.25", "videoname": "parrot video"},
+                    {"timestamp": "20.5", "value": "0.57", "videoname": "lizard video"},
+                    {"timestamp": "22.8", "value": "0.91", "videoname": "another cat video"}]}
+    # commented original calling-json-function:
+    #data = d.collect_posts()
+    jsonData2 = json.loads(json.dumps(data2)) # type == dict
+    df = pd.DataFrame(jsonData2)
+    data_valence = df.objects.apply(lambda x: pd.Series(x))
+
+    TOOLS = 'save,pan,box_zoom,reset,wheel_zoom,hover'
+    p = figure(title="Valence ratings by timestamp", y_axis_type="linear", x_range=(0, 23), y_range=(-1, 1), plot_height = 400,
+           tools = TOOLS, plot_width = 900)
+    p.xaxis.axis_label = 'Timestamp (seconds)'
+    p.yaxis.axis_label = 'Valence rating'
+    p.line(data_valence.timestamp, data_valence.value,line_color="purple", line_width = 3)
+
+    source = ColumnDataSource(data={
+    'timestamp': data_valence.timestamp,
+    'value': data_valence.value,
+    'videoname': data_valence.videoname})
+
+    p.select_one(HoverTool).tooltips = [
+    ('timestamp', '@x'),
+    ('Rating of valence', '@y'),
+    ('videoname', '@videoname')
+    ]
+
+    script, div = components(p)
+    return render_template("line_chart.html", title="Line Chart", the_div=div, the_script=script)
+
+
 
 @app.route('/collect_data')
 def collect_data():
