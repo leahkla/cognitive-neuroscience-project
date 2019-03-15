4ca4808f7d94901a468e25f76ae16dec9d35e373
Moritz Lange <moritz.lange@uni-ulm.de>
2019-03-15 23:00:50 +0200

    Removed Flask-Session dependency
    Since it seems to work just as well without the package (i.e. with local cookies, if I'm not mistaken).

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,8 @@ This  file initiates the flask app.
 
 from flask import Flask
 from app.config import Config
-# from flask.ext.session import Session
-from flask_session.__init__ import Session
 
-# SESSION_TYPE = 'redis'
-# app.config.from_object(__name__)
 app = Flask(__name__)
 app.config.from_object(Config)
-
-sess = Session()
 
 from app import routes
