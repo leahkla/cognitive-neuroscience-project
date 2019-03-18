74b1c4110062865a77647b5c0b0aa242827b4236
Moritz Lange <moritz.lange@uni-ulm.de>
2019-03-18 09:52:39 +0200

    Added a bootstrap js import
    Now some functionalities work that didn't before (like dropdown menus or
    closeable alert buttons)

--- app/user/routes.py
+++ app/user/routes.py
@@ -16,9 +16,13 @@ from app.functionalities import check_access_right
 def user():
     """
     Display the main user page.
+    [The videos that can be accessed have to be defined with name and vimeo
+    ID in a dictionary here.]
 
     This is not accessible for somebody with the role researcher.
     :return: Main webpage
     """
+    # videos = {'Omelette':'65107797', 'Happiness':'28374299'}
+
     check_access_right(forbidden='researcher', redirect_url='researcher.chart')
-    return render_template('user/user.html')
+    return render_template('user/user.html')  # , videos=videos)
