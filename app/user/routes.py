8fdfb0f7a065fdb94b74de3a2099366dcda918b6
Moritz Lange <moritz.lange@uni-ulm.de>
2019-04-19 18:24:52 +0300

    Added functionality to have multiple videos
    The videos can be listed in video_conf.txt and the user can choose which one to watch.

--- app/user/routes.py
+++ app/user/routes.py
@@ -6,7 +6,7 @@ It has only the webpages belonging to the user blueprint,
 i.e. those belonging to the user interface.
 """
 
-from flask import render_template
+from flask import render_template, request, flash
 
 from app.user import bp
 from app.functionalities import check_access_right
@@ -25,4 +25,19 @@ def user():
     # videos = {'Omelette':'65107797', 'Happiness':'28374299'}
 
     check_access_right(forbidden='researcher', redirect_url='researcher.chart')
-    return render_template('user/user.html')  # , videos=videos)
+
+    vid_file = 'app/user/video_conf.txt'
+    with open(vid_file) as f:
+        videos = f.readlines()
+    vid_list = [x.strip() for x in videos if x.strip() if
+                (x.strip()[0] is not '#')]
+    vid_dict = dict([map(str.strip, i.split(':')) for i in vid_list])
+
+    cur_vid_id = request.args.get('vid')
+    if not cur_vid_id:
+        currentVideo = vid_list[0].split(':')
+    else:
+        currentVideo = [cur_vid_id, vid_dict[cur_vid_id]]
+
+    return render_template('user/user.html', vid_dict=vid_dict,
+                           currentVideo=currentVideo)
